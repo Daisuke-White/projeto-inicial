73ed9ce041bc8990f1c4844428797e8d021f0df4
Daisuke-White <gamersuke2@gmail.com>
2024-03-15 08:23:01 -0300

    projeto-inicial

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar,false);
-
+var nome = document.getElementById("nome");
 function alertar(event){
-    alert("Você clicou no botão!!! " + event);
+    alert("Você clicou no botão!!! " + " " + nome.value);
 
     var numero = 7;
     var resultado = numero % 2;
